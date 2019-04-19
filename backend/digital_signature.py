75768fc1512b63b4b4a77ff7eca50579bbb03351
azisak <azisadikuncoro@gmail.com>
2019-04-19 09:40:24 +0700

    Fix encrypt problem

--- backend/digital_signature.py
+++ backend/digital_signature.py
@@ -12,7 +12,10 @@ def sign_message(message, ecc, point_basis, public_key):
     if (type(message) != bytes):
         message = bytes(message, "utf-8")
 
-    signature = SHA3_512(message.encode())
+    signature = keccak.SHA3_512(message)
+    
+    if (type(signature) == bytearray):
+        signature = signature.hex()
 
     signature = signature.encode('utf-8')
     signature = ecc.encrypt_message(signature, point_basis, public_key)
@@ -30,7 +33,10 @@ def verified(message, signature, ecc, private_key):
     if (type(message) != bytes):
         message = bytes(message, "utf-8")
 
-    m_signature = SHA3_512(message.encode())
+    m_signature = keccak.SHA3_512(message)
+
+    if (type(m_signature) == bytearray):
+        m_signature = m_signature.hex()
 
     signature = ecc.decrypt_message(signature, private_key)
     signature = signature.decode('utf-8')
