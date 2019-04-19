ce1eee980d6e60a67d640a52ffb801ae30ac3c18
Radiyya Dwisaputra <radiyyasaputra@gmail.com>
2019-04-19 09:19:12 +0700

    Add rypyth files

---
+++ backend/rypyth/Function.py
@@ -0,0 +1,142 @@
+from rypyth.Constant import S_BOX, NON_FIBONACCI_NUM
+import math
+
+
+class Function:
+    def __init__(self, binary_plain, binary_key):
+        if len(binary_plain) != 64 or len(binary_key) != 64: raise Exception()
+        self.binary_plain = binary_plain
+        self.binary_key = binary_key
+
+        self.REPETITION = 8
+
+    def __substitute_using_non_fibonacci_index(self):
+        string = list(self.binary_plain)
+        for num in NON_FIBONACCI_NUM:
+            string[num] = "1" if string[num] == "0" else "0"
+
+        self.binary_plain = "".join(string)
+
+    def __substitute_using_s_box(self):
+        string = split_string_into_list_of_length_n(self.binary_plain, 8)
+        for index in range(len(string)):
+            string[index] = str(S_BOX[int(string[index], 2)])
+        for index in range(len(string)):
+            string[index] = "{:08b}".format(int(string[index]))
+
+        self.binary_plain = "".join(string)
+
+    def __transpose(self):
+        string = split_string_into_list_of_length_n(self.binary_plain, 8)
+        string[0], string[3], string[4], string[7] = string[3], string[0], string[7], string[4]
+
+        self.binary_plain = "".join(string)
+
+    def __slide(self):
+        string_on_the_left = split_string_into_list_of_length_n(self.binary_plain[:30], 5)
+        string_on_the_right = split_string_into_list_of_length_n(self.binary_plain[34:], 5)
+        string_on_the_center = self.binary_plain[30:-30]
+        for index in range(len(string_on_the_left)):
+            string_on_the_left[index] = switch_string_of_five(string_on_the_left[index])
+        for index in range(len(string_on_the_right)):
+            string_on_the_right[index] = switch_string_of_five(string_on_the_right[index])
+        self.binary_plain = "".join(string_on_the_left) + string_on_the_center + "".join(string_on_the_right)
+
+    def __xor_with_key(self):
+        string = split_string_into_list_of_length_n(self.binary_plain, 4)
+        string_key = split_string_into_list_of_length_n(self.binary_key, 4)
+        for index in range(len(string)):
+            string[index] = "{:04b}".format(int(string[index], 2) ^ int(string_key[len(string_key) - 1 - index], 2))
+
+        self.binary_plain = "".join(string)
+
+    def __generate_round_key(self):
+        string = split_string_into_list_of_length_n(self.binary_plain, 8)
+        string_key = split_string_into_list_of_length_n(self.binary_key, 8)
+        for index in range(len(string_key)):
+            num_1, num_2 = int(string[index], 2), int(string_key[index], 2)
+            num_result = int(math.floor(math.sqrt((num_1 * num_1) + (num_2 * num_2)))) % 256
+            string_key[index] = "{:08b}".format(num_result)
+
+        self.binary_key = "".join(string_key)
+
+    def run(self):
+        for i in range(self.REPETITION):
+            self.__substitute_using_non_fibonacci_index()
+            self.__substitute_using_s_box()
+            self.__transpose()
+            self.__slide()
+            self.__xor_with_key()
+        return self.binary_plain
+
+
+def split_string_into_list_of_length_n(string, n):
+    if (len(string) % n) != 0: raise Exception()
+    return [string[i:i + n] for i in range(0, len(string), n)]
+
+def switch_string_of_five(string):
+    if len(string) != 5: raise Exception()
+    return string[3] + string[4] + string[2] + string[0] + string[1]
+
+def change_ascii_to_bits(string):
+    result = ''
+    i=0
+    for x in string:
+        if type(x) is int:
+            result += format(x,'b').zfill(8)
+        else:
+            result += format(ord(x), 'b').zfill(8)
+        i+=1
+    
+    return result
+
+def change_bits_to_ascii(bits):
+    result = ''
+    ascii = split_string_into_list_of_length_n(bits,8)
+    for i in ascii:
+        result += chr(int(i,2))
+
+    return result
+
+# PKCS5 padding
+def pkcs5_padding(bplain):    
+    if len(bplain) % 128 != 0:
+        # lack in bytes
+        lack = int((128 - (len(bplain)%128)) / 8)
+        for i in range(lack):
+            bplain += '{0:08b}'.format(lack)
+    else:
+        for i in range(16):
+            bplain += '{0:08b}'.format(128)
+    bplain_arr = split_string_into_list_of_length_n(bplain,128)
+    return bplain_arr
+
+def last_byte_check(btext):
+    btext = split_string_into_list_of_length_n(btext,8)
+    count = int(btext[-1],2)
+    bits = ''.join(btext[:len(btext)-count])
+    result = change_bits_to_ascii(bits)
+    return result
+
+def read_input_bytes(fileinput):
+    # plaintext = bytes('', 'utf-8')
+    with open(fileinput, "rb") as f:
+        byte_file = f.read()
+    plaintext = byte_file
+    return plaintext
+
+def save_output_bytes(data, outputname):
+    # if hasattr(data, 'encode'):
+    #     data = data.encode('utf-8')
+    with open(outputname, "wb+") as f:
+        f.write(data)
+
+if __name__ == "__main__":
+    src = "1111000011110000111100001111000011110000111100001111000011110000"
+    key = "0011001100110011001100110011001100110011001100110011001100110011"
+    print(read_input_bytes('decrypt.jpg'))
+    input_ascii = ''
+    for i in read_input_bytes('small_cat.jpg'):
+        input_ascii += chr(i)
+    save_output_bytes(input_ascii,'result.txt')
+    f = Function(input_ascii, key)
---
+++ backend/rypyth/Modes.py
@@ -0,0 +1,204 @@
+from rypyth.Function import *
+from rypyth.Feistel import *
+import random
+
+class Modes:
+    def __init__(self, plain, key):
+        self.plain = plain
+        if len(key)>8:
+            random.seed(key)
+            self.key = ''.join(random.sample(key[:8],8))
+        else:
+            # key < 8 bytes
+            padding = 8-len(key)
+            random.seed(padding)
+            temp_key=''
+            while len(temp_key) < 8:
+                temp_key = key + ''.join(random.sample(key,len(key)))
+            self.key = temp_key[:8]
+        self.bplain = change_ascii_to_bits(self.plain)
+        self.bplain_encrypt = pkcs5_padding(self.bplain)
+        self.bkey = change_ascii_to_bits(self.key)
+    
+
+    def ecb_encrypt(self):
+        result = ''
+        for i in self.bplain_encrypt:
+            f = Feistel(i,self.bkey)
+            result += f.encrypt()
+        return result
+
+    def ecb_decrypt(self):
+        cipher_arr = split_string_into_list_of_length_n(self.bplain,128)
+        result = ""
+        for i in cipher_arr:
+            f = Feistel(i, self.bkey)
+            result += f.decrypt()
+        result = last_byte_check(result)
+        return result
+    
+    def cbc_encrypt(self):
+        iv = 'rypythencryption'
+        index = 0
+        result = ''
+
+        for i in self.bplain_encrypt:
+            if index == 0:
+                c = iv
+            i = bin(int(i,2) ^ int(change_ascii_to_bits(c),2))[2:].zfill(128)
+            f = Feistel(i,self.bkey)
+            c = f.encrypt()
+            result += c
+            index += 1
+        
+        return result
+    
+    def cbc_decrypt(self):
+        input_bcipher = split_string_into_list_of_length_n(self.bplain,128)
+        iv = 'rypythencryption'
+        index = 0
+        result = ''
+
+        for i in input_bcipher:
+            f = Feistel(i,self.bkey)
+            if index == 0:
+                c = change_ascii_to_bits(iv)
+            p = f.decrypt()
+            p = bin(int(p,2) ^ int(c,2))[2:].zfill(128)
+            c = i
+            result += p
+            index += 1
+
+        result = last_byte_check(result)
+        return result
+
+    def cfb_encrypt(self):
+        n = 1 #size of unit in bytes
+        iv = 'rypythencryption'
+        bitplain = ''
+        for i in self.bplain_encrypt:
+            bitplain += i
+        bitplain = split_string_into_list_of_length_n(bitplain,(n*8))
+        index = 0
+        result = ''
+
+        for i in bitplain:
+            if index == 0:
+                x = change_ascii_to_bits(iv)
+            f = Feistel(x, self.bkey)
+            c = bin(int(change_ascii_to_bits(f.encrypt()[0]),2) ^ int(i,2))[2:].zfill(n*8)
+            result += c
+            x = x[:(len(x)-(n*8))] + c
+            index += 1
+        
+        result = change_bits_to_ascii(result)
+        return result
+
+    def cfb_decrypt(self):
+        n = 1 #size of unit in bytes
+        iv = 'rypythencryption'
+        bitplain = ''
+        # not using self.bplain because not using padding
+        for i in self.bplain:
+            bitplain += i
+        bitplain = split_string_into_list_of_length_n(bitplain,(n*8))
+        index = 0
+        result = ''
+
+        for i in bitplain:
+            if index == 0:
+                x = change_ascii_to_bits(iv)
+            f = Feistel(x, self.bkey)
+            c = bin(int(change_ascii_to_bits(f.encrypt()[0]),2) ^ int(i,2))[2:].zfill(n*8)
+            result += c
+            x = x[:(len(x)-(n*8))] + i
+            index += 1
+
+        result = last_byte_check(result)
+        return result
+    
+    def ofb_encrypt(self):
+        n = 1 #size of unit in bytes
+        iv = 'rypythencryption'
+        bitplain = ''
+        for i in self.bplain_encrypt:
+            bitplain += i
+        bitplain = split_string_into_list_of_length_n(bitplain,(n*8))
+        index = 0
+        result = ''
+
+        for i in bitplain:
+            if index == 0:
+                x = change_ascii_to_bits(iv)
+            f = Feistel(x, self.bkey)
+            m = change_ascii_to_bits(f.encrypt()[0])
+            c = bin(int(m,2) ^ int(i,2))[2:].zfill(n*8)
+            result += c
+            x = x[:(len(x)-(n*8))] + m
+            index += 1
+        
+        result = change_bits_to_ascii(result)
+        return result
+
+    def ofb_decrypt(self):
+        n = 1 #size of unit in bytes
+        iv = 'rypythencryption'
+        bitplain = ''
+        # not using self.bplain_encrypt because not using padding
+        for i in self.bplain:
+            bitplain += i
+        bitplain = split_string_into_list_of_length_n(bitplain,(n*8))
+        index = 0
+        result = ''
+
+        for i in bitplain:
+            if index == 0:
+                x = change_ascii_to_bits(iv)
+            f = Feistel(x, self.bkey)
+            m = change_ascii_to_bits(f.encrypt()[0])
+            c = bin(int(m,2) ^ int(i,2))[2:].zfill(n*8)
+            result += c
+            x = x[:(len(x)-(n*8))] + m
+            index += 1
+
+        result = last_byte_check(result)
+        return result
+
+    def counter_encrypt(self):
+        iv = 'rypythen'
+        counter = int('1011000111110000101010101111111100000001000101011010111110001111',2)
+        index = 0
+        result = ''
+
+        for i in self.bplain_encrypt:
+            if index == 0:
+                x = change_ascii_to_bits(iv) + bin(counter)[2:]
+            f = Feistel(x, self.bkey)
+            m = change_ascii_to_bits(f.encrypt())
+            c = bin(int(m,2) ^ int(i,2))[2:].zfill(128)
+            result += c
+            x = change_ascii_to_bits(iv) + bin(counter + 3)[2:]
+            index += 1
+        
+        result = change_bits_to_ascii(result)
+        return result
+
+
+    def counter_decrypt(self):
+        iv = 'rypythen'
+        counter = int('1011000111110000101010101111111100000001000101011010111110001111',2)
+        index = 0
+        result = ''
+        bitplain = split_string_into_list_of_length_n(self.bplain, 128)
+
+        for i in bitplain:
+            if index == 0:
+                x = change_ascii_to_bits(iv) + bin(counter)[2:]
+            f = Feistel(x, self.bkey)
+            m = change_ascii_to_bits(f.encrypt())
+            c = bin(int(m,2) ^ int(i,2))[2:].zfill(128)
+            result += c
+            x = change_ascii_to_bits(iv) + bin(counter + 3)[2:]
+            index += 1
+        result = last_byte_check(result)
+        return result
